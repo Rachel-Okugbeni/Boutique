ef25a6727871f5194e3dc80cedb0118904f683b0
Rachel O <68948057+Rachel-Okugbeni@users.noreply.github.com>
2020-10-06 16:29:10 +0100

    inital commit + home page layout & design, image slide show + newsletter form

---
+++ slideshow.js
@@ -0,0 +1,37 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("banner-slide");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+      slideIndex = 1
+    }
+  if (n < 1) {
+      slideIndex = slides.length
+    }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); 
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex-1].className += " active";
+}
