88d898b2477537facb2891da71c8b8c2fb3a7846
Rachel O <68948057+Rachel-Okugbeni@users.noreply.github.com>
2020-10-07 19:01:37 +0100

    Added jewellery product page

--- slideshow.js
+++ slideshow.js
@@ -28,7 +28,7 @@ function showSlides(n) {
   slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); 
+    setTimeout(showSlides, 4000); 
 
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
